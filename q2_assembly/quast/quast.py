87b5e02f71c562a262b6ca819e74c8e97a707096
Michal Ziemski <mziemski@ethz.ch>
2023-01-04 16:05:55 +0100

    FIX: enable multi-processing on Linux (#33)
    * FIX: enable multi-processing on Linux
    * fixup! FIX: enable multi-processing on Linux
    * update copyright year
    * add comment with quast issue reference

--- q2_assembly/quast/quast.py
+++ q2_assembly/quast/quast.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# Copyright (c) 2022, QIIME 2 development team.
+# Copyright (c) 2023, QIIME 2 development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
@@ -9,6 +9,7 @@
 import glob
 import json
 import os
+import platform
 import subprocess
 import tempfile
 from distutils.dir_util import copy_tree
@@ -54,13 +55,10 @@ def _process_quast_arg(arg_key, arg_val):
     """
     if isinstance(arg_val, bool) and arg_val:
         return [_construct_param(arg_key)]
-    elif arg_key == "threads" and (not arg_val or arg_val > 1):
-        # TODO: this needs to be fixed (to allow multiprocessing)
-        print(
-            "Multiprocessing is currently not supported. Resetting "
-            "number of threads to 1."
-        )
-        return [_construct_param(arg_key), "1"]
+    elif arg_key == "threads" and arg_val > 1 and platform.system() != "Linux":
+        # This is a limitation in quast that is documented in this issue:
+        # https://github.com/ablab/quast/issues/175
+        raise ValueError("Multiprocessing is currently only supported on Linux.")
     elif not isinstance(arg_val, list):
         return [_construct_param(arg_key), str(arg_val)]
     else:
