0cb5aa66cfebad12f35432b43a6f6bf6e8786553
Oddant1 <oddant1@hotmail.com>
2023-09-12 15:12:55 -0700

    partition_contigs

--- q2_assembly/megahit/helpers.py
+++ q2_assembly/megahit/helpers.py
@@ -10,6 +10,23 @@ from q2_types_genomics.per_sample_data import ContigSequencesDirFmt
 from qiime2.util import duplicate
 
 
+def partition_contigs(
+    contigs: ContigSequencesDirFmt, num_partitions: int = None
+) -> ContigSequencesDirFmt:
+    collection = {}
+    for sample_fp in contigs.path.iterdir():
+        result = ContigSequencesDirFmt()
+        # These paths are defined in the ContigSequencesDirFmt class as
+        # {sample_id}_contigs,(fa | fasta). This should get the id from a name
+        # like that
+        sample_id = sample_fp.name.split("_contigs")[0]
+        duplicate(sample_fp, result.path / sample_fp.name)
+
+        collection[sample_id] = result
+
+    return collection
+
+
 def collate_contigs(contigs: ContigSequencesDirFmt) -> ContigSequencesDirFmt:
     collated_contigs = ContigSequencesDirFmt()
 
