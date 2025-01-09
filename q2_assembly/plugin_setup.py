4881d24de5eca1716882a43e32235555d56ff7db
Michal Ziemski <mziemski@ethz.ch>
2025-01-09 13:49:51 +0100

    fixup! Add tests

--- q2_assembly/plugin_setup.py
+++ q2_assembly/plugin_setup.py
@@ -21,7 +21,6 @@ from q2_types.per_sample_sequences import (
     SingleBowtie2Index,
 )
 from q2_types.sample_data import SampleData
-from qiime2 import Metadata
 from qiime2.core.type import Bool, Choices, Properties, Str, TypeMap, Visualization
 from qiime2.plugin import Citations, Collection, Int, List, Plugin, Range
 
